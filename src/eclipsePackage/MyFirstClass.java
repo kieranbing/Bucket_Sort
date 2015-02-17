7b7bbdccc985b11e4eec1e826ac681f9a25ef7a9
Aritz <Aritz@Cortain>
2015-02-17 10:52:34 -0500

    Testing

---
+++ src/eclipsePackage/MyFirstClass.java
@@ -0,0 +1,42 @@
+package eclipsePackage;
+
+//import javax.swing.JOptionPane;
+
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true){
+			System.out.print("al");
+		}
+		/*
+		for (int i=0;i<=60;i++){
+			if(i%10==0){
+				continue;
+			}
+			System.out.print("THIS IS #"+i+"\n");
+		}
+		*/
+		/*
+		System.out.print("Hello\nworld\n\"These are quotes\"\0aaa \\\n\n");
+		int g = -1;
+		System.out.println("User must enter name");
+		JOptionPane.showMessageDialog(null, "Please enter your name!");
+		while(g<0){
+			String input = JOptionPane.showInputDialog("Enter your name");
+			if (input != null && input.length()>0){
+				g++;
+				System.out.print("User has entered ");
+				System.out.println(input);
+				JOptionPane.showMessageDialog(null, "Thanks, "+input+". Your credit card has been charged.");
+			}else{
+				System.out.println("Invalid name. Retrying");
+				JOptionPane.showMessageDialog(null,"You haven't entered a name!\nPlease enter your name to continue.","Warning", JOptionPane.WARNING_MESSAGE);
+			}
+		}
+		*/
+		
+	}
+
+}
