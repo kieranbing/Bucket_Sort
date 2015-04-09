021da3cf9f1f9d3113e6f1c16a3f13721b1d73f1
Kieran Bingham <kbingham.ca@gmail.com>
2015-04-08 22:44:41 -0400

    Added file read/write project v.1

---
+++ fileInteraction/src/test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Family
+ */
+import java.io.File;
+
+public class test {
+    void createTemp() {
+        File tempfile = new File("tempfile.txt");
+    }
+}
